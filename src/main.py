1b8a9d5c0cb8ac12e2da5283449d8ea08b3da2fd
Just Student <just.student.lab@mail.ru>
2023-04-08 02:31:52 +0300

    V1

--- src/main.py
+++ src/main.py
@@ -19,9 +19,13 @@ def main(settings: dict):
     indicators_results = []
     url_scan = urlscan.api.Urlscan()
     try:
-        urlscan_result = url_scan.search_title_by_keywords(
-            settings["keywords"]["title"]
-        )
+        urlscan_result = []
+        if settings["keywords"]["enable"]:
+            urlscan_result = url_scan.search_title_by_keywords(
+                settings["keywords"]["title"]
+            )
+        if settings["ips"]["enable"]:
+            urlscan_result += url_scan.search_urls_by_ip(settings["ips"]["ips"])
         indicators_results += urlscan_result
         logger.info("get data from Urlscan [len: %s]" % len(urlscan_result))
     except Exception as error:
@@ -29,8 +33,11 @@ def main(settings: dict):
 
     net = netlas_api.api.Netlas()
     try:
-        netlas_result = net.favicon_search(settings["favicons"]["sha256hash"])
-        netlas_result += net.title_search(settings["keywords"]["title"])
+        netlas_result = []
+        if settings["favicons"]["enable"]:
+            netlas_result = net.favicon_search(settings["favicons"]["sha256hash"])
+        if settings["keywords"]["enable"]:
+            netlas_result += net.title_search(settings["keywords"]["title"])
         indicators_results += netlas_result
         logger.info("get data from Netlas [len: %s]" % len(netlas_result))
     except Exception as error:
@@ -38,8 +45,11 @@ def main(settings: dict):
 
     phish_stats = phishstats.api.PhishStats()
     try:
-        phish_stats_results = phish_stats.search_titles(settings["keywords"]["title"])
-        indicators_results += phish_stats_results
+        if settings["keywords"]["enable"]:
+            phish_stats_results = phish_stats.search_titles(
+                settings["keywords"]["title"]
+            )
+            indicators_results += phish_stats_results
         if settings["ips"]["enable"]:
             phish_stats_results = phish_stats.search_urls_by_ip(settings["ips"]["ips"])
             indicators_results += phish_stats_results
@@ -49,11 +59,16 @@ def main(settings: dict):
 
     _shodan = shodan_api.api.ShodanApi()
     try:
-        shoda_results = _shodan.search_hosts_by_favicon(
-            settings["favicons"]["murmurhash"]
-        )
-        shoda_results += _shodan.search_title_by_keywords(settings["keywords"]["title"])
-        indicators_results += shoda_results
+        shodan_results = []
+        if settings["favicons"]["enable"]:
+            shodan_results = _shodan.search_hosts_by_favicon(
+                settings["favicons"]["murmurhash"]
+            )
+        if settings["keywords"]["enable"]:
+            shodan_results += _shodan.search_title_by_keywords(
+                settings["keywords"]["title"]
+            )
+        indicators_results += shodan_results
         logger.info("get data from Shodan [len: %s]" % len(indicators_results))
     except Exception as error:
         logger.error("get data from Shodan - %s" % error)
